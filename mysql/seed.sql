c7a2fe9e4fe9c7958d51f79636261f1e0d1479d8
warmup232 <tglee716@gmail.com>
2026-08-25 00:06:21 +0900

    Add seed data for departments and employees

---
+++ mysql/seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO departments (name)
+VALUES
+('IT'),
+('Database'),
+('Infrastructure');
+INSERT INTO employees (name, email, department_id)
+VALUES
+('Kim Minsoo', 'minsoo@example.com', 1),
+('Lee Jisoo', 'jisoo@example.com', 2),
+('Park Junho', 'junho@example.com', 3);
